6b926ebbf9c4b123e9318805f807cb655f6b9bf7
Georg Tremmel <g@trembl.org>
2024-07-26 12:28:40 +0900

    Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,php,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ["Helvetica Neue", 'ui-sans-serif', 'system-ui',' -apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto, Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji"],
+    },
+    /*
+    screens: {
+      
+      sm: '640px', // default 640px
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    */
+    extend: {
+      colors: {
+        'rot': '#D00025',
+      }
+    },
+  },
+  plugins: [],
+  safelist: []
+}
+
