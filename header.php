4c66cb267efcfb67e6e36c569355c7f2eea8a1e7
Georg Tremmel <g@trembl.org>
2024-08-09 12:28:30 +0900

    Link Color Test

--- header.php
+++ header.php
@@ -7,19 +7,25 @@
   <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" />
   <style>
 <?php
+
 if (get_field('farbe')) {
-  echo "#background { background: ". get_field('farbe') ."}";
+  //echo "#background { background: ". get_field('farbe') ."}";
+  $color = get_field('farbe');
 } else if (get_field('farbe', $post->post_parent)) {
-  echo "#background { background: ". get_field('farbe', $post->post_parent) ."}";
+  //echo "#background { background: ". get_field('farbe', $post->post_parent) ."}";
+  $color = get_field('farbe', $post->post_parent);
+
 }
 ?>
+    a, a:visited {
+      background-color: <?php echo $color; ?>;
+    }
   </style>
-
 </head>
 <body class="">
   <div id="background">
     <header>
-      <div class="text-4xl lg:text-lg font-normal px-8 py-1 bg-white text-center">
+      <div class="text-lg font-normal px-8 py-1 bg-white text-center">
         Seminar zur österreichischen Gegenwartsliteratur in Japan オーストリア現代文学ゼミナール
       </div>
       <div class="menu">
