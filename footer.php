b3a0871fe60cb617ee945909c55e1cd0e044703c
Georg Tremmel <g@trembl.org>
2024-11-05 19:38:57 +0900

    New Logos

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
   <footer class= >
     <div class="p-8 container mx-auto">
       <div class="text-lg italic text-center">
-        Mit Unterstützung des Österreichischen Kulturforums Tokio und des 
-        <img class="inline" src="https://www.onsem.info/images/logos/oeadlogo.jpg" height="15" width="60"><br />
-        <img class="mx-auto" src="https://www.onsem.info/images/logos/okf_horizontal_logotype_digital_black.png" width="450">
+        Mit Unterstützung des Österreichischen Kulturforums Tokio und des
+        <img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/OeAD_LogoSolo.jpg" height="15" width="60"><br />
+        <img class="mx-auto" src="<?php echo bloginfo('template_directory'); ?>/images/okf_horizontal_logotype_digital_black.png" width="450">
       </div>
     </div>
     <div class="menu menu-bottom">
@@ -13,7 +13,7 @@
   <?php showMenu('top-menu'); ?>
     </div>
   </footer>
-    
+
 </div><!--end page -->
 <?php wp_footer(); ?>
 
