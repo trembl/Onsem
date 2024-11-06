28fa2e7fd315d8c0ce7f594a50045924dad7bb65
Georg Tremmel <g@trembl.org>
2024-11-06 17:18:36 +0900

    Logo

--- footer.php
+++ footer.php
@@ -3,9 +3,13 @@
   <footer class= >
     <div class="p-8 container mx-auto">
       <div class="text-lg italic text-center">
-        Mit Unterstützung des Österreichischen Kulturforums Tokio und des
-        <img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/OeAD_LogoSolo.jpg" height="15" width="60"><br />
-        <img class="mx-auto" src="<?php echo bloginfo('template_directory'); ?>/images/okf_horizontal_logotype_digital_black.png" width="450">
+        Mit Unterstützung des <a href="https://www.austrocult.jp">Österreichischen Kulturforums Tokio</a> und des <a href="https://oead.at">OEAD</a>.<br />
+        <a href="https://www.austrocult.jp">
+          <img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/okf_horizontal_logotype_digital_black.png" width="350">
+        </a>
+        <a href="https://oead.at">
+          <img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/OeAD_LogoSolo.jpg" width="150">
+        </a>
       </div>
     </div>
     <div class="menu menu-bottom">
