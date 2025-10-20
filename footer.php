2fc34e4d857f70608dc3ef5af9e5d518b58d98ed
Georg Tremmel <g@trembl.org>
2025-10-20 16:21:32 +0900

    Logo Updates

--- footer.php
+++ footer.php
@@ -4,7 +4,21 @@
     <div class="p-8 container mx-auto">
       <div class="text-lg italic text-center">
         Mit Unterstützung des <a href="https://www.austrocult.jp">Österreichischen Kulturforums Tokio</a> und des <a href="https://oead.at">OeAD</a>.<br />
-        <a href="https://www.austrocult.jp"><img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/okf_horizontal_logotype_digital_black.png" width="350"></a><a href="https://oead.at"><img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/OeAD_LogoSolo.jpg" width="150"></a>
+        <a href="https://www.austrocult.jp"><img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/OKF_horizontal_logotype_digital_black_acf.png" width="350"></a><a href="https://oead.at"><img class="inline" src="<?php echo bloginfo('template_directory'); ?>/images/OeAD_LogoSolo.jpg" width="150"></a>
+      </div>
+    </div>
+    <div class="menu menu-bottom">
+      <a href="<?php echo home_url(); ?>">♨️ Onsem 📖</a>
+  <?php showMenu('top-menu'); ?>
+    </div>
+  </footer>
+
+</div><!--end page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
+hp echo bloginfo('template_directory'); ?>/images/OeAD_LogoSolo.jpg" width="150"></a>
       </div>
     </div>
     <div class="menu menu-bottom">
